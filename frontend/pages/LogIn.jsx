89c8735055ae1b762d2a0acabce1d55dc55c5a11
munaf-ali1 <143165045+munaf-ali1@users.noreply.github.com>
2025-12-23 15:39:53 +0530

    add backend url in login page

--- frontend/pages/LogIn.jsx
+++ frontend/pages/LogIn.jsx
@@ -28,7 +28,7 @@ function Login() {
       setLoading(true);
 
       const res = await axios.post(
-        "http://localhost:8000/api/auth/login",
+        "https://lms-backend-3z5b.onrender.com/api/auth/login",
         formData,
         { withCredentials: true }
       );
